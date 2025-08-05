faebe811fc4177ead75f5a1d3caf7fedac96edac
Shara Ann Charles <sharaanncharles@gmail.com>
2025-08-05 13:07:21 +0530

    Add basic_queries.sql with example queries

---
+++ queries/sprint_queries.sql
@@ -0,0 +1,8 @@
+-- 1. List all customers
+SELECT * FROM customers;
+
+-- 2. Show products with price greater than 100
+SELECT * FROM products WHERE price > 100;
+
+-- 3. Count total orders
+SELECT COUNT(*) FROM orders;
